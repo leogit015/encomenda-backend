41ccfb426c7a58ddbd193809e4809059aab575af
leogit015 <olivercarvalho15@gmail.com>
2024-05-11 23:02:43 -0300

    configurando ambiente

---
+++ src/test/java/com/encomenda/EncomendaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encomenda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncomendaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
